ef42d83975f8135b6405b4192f07aabad8617053
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2021-01-06 22:08:09 +0900

    Add files via upload

--- This_is_CodingTest/Gready_Algorithm/1일 될때까지.py
+++ This_is_CodingTest/Gready_Algorithm/1일 될때까지.py
@@ -1,17 +1,18 @@
 import sys
 import time
+
 n, k = map(int,sys.stdin.readline().split())
 count = 0
 
-while True:
-    if n%k !=0:
+while n>=k:
+    while n % k !=0:
         n-=1
-        count +=1
+        count += 1
+    n //= k
+    count += 1
 
-    n //=k
-    count +=1
-    if n==1:
-        break
+
+
 
 
 print(count)
