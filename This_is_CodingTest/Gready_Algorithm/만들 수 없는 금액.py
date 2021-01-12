a02849c5febe7128bcb6c0ef85f20f0c70234d6e
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2021-01-12 10:01:05 +0900

    Update 만들 수 없는 금액.py

--- This_is_CodingTest/Gready_Algorithm/만들 수 없는 금액.py
+++ This_is_CodingTest/Gready_Algorithm/만들 수 없는 금액.py
@@ -1,11 +1,13 @@
-n = int(input())
+import sys
 
-data = list(map(int,input().split()))
-data.sort()
+N = map(int,sys.stdin.readline())
+coin = list(map(int,sys.stdin.readline().split()))
+
+coin.sort()
 
 target = 1
 
-for x in data:
+for x in coin:
     if target < x:
         break
     target += x
