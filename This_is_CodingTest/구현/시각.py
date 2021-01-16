69ff39b77d7fda0cadbfcc4ce0d6ac34a41d8406
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2021-01-16 21:44:16 +0900

    Update 시각.py

--- This_is_CodingTest/구현/시각.py
+++ This_is_CodingTest/구현/시각.py
@@ -5,9 +5,9 @@ n = int(sys.stdin.readline())
 count = 0
 for i in range(n+1):
     for j in range(60):
-        for n in range(60):
+        for k in range(60):
             # 매 시각 안에 '3'이 포함되어 있다면 카운트 증가
-            if '3' == str(i) + str(j) + str(k):
+            if '3' in str(i) + str(j) + str(k):
                 count += 1
 
 
