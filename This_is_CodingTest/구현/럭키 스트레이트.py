fd2684e9c87b582ddc3b17297f1d8f2e66e03c26
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2021-01-20 21:52:47 +0900

    Add files via upload

---
+++ This_is_CodingTest/구현/럭키 스트레이트.py
@@ -0,0 +1,37 @@
+import sys
+#1
+n = sys.stdin.readline().split()
+
+count = len(n) // 2
+
+result1 = 0
+result2 = 0
+for i in range(len(n)):
+    if i < count:
+        result1 += int(n[i])
+    else:
+        result2 += int(n[i])
+
+if result1 == result2:
+    print("Lucky")
+else:
+    print("READY")
+
+#2
+n = input()
+length = len(n) # 점수값의 총 자릿수
+summary = 0
+
+# 왼쪽 부분의 자릿수 합 더하기
+for i in range(length // 2):
+    summary += int(n[i])
+
+# 오른쪽 부분의 자릿수 합 빼기
+for i in range(lengh // 2, length):
+    summary -= int(n[i])
+
+# 왼쪽 부분과 오른쪽 부분의 자릿수 합이 동일한지 검사
+if summary == 0 :
+    print("LUCKY")
+else:
+    print("READY")
