654617449135f4cfd175a353143f6c9ee6e7a8b6
Anchangwan <56478474+ANchangwan@users.noreply.github.com>
2021-05-07 20:51:49 +0900

    Add files via upload

---
+++ This_is_CodingTest/그래프 이론/Algorithm_Question/커리큘럼.py
@@ -0,0 +1,34 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+graph = [[] for _ in range(n+1)]
+indegree = [0] * (n+1)
+time = [0] * (n+1)
+
+for i in range(1,n+1):
+    data = list(map(int,input().split()))
+    time[i] = data[0]
+    for x in data[1:-1]:
+        print(x)
+        indegree[i] += 1
+        graph[x].append(i)
+
+def topology_sort():
+    result = copy.deepcopy(time)
+    q = deque()
+
+    while q:
+        now = q.popleft()
+        for i in graph[now]:
+            result[i] = max(result[i],result[now] + time[i])
+            indegree -= 1
+            if indegree[i] == 0:
+                q.append(i)
+
+    for i in range(1,n+1):
+        print(result[i])
+
+topology_sort()
