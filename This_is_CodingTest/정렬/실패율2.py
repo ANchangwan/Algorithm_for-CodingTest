0604010546c93f3d94dca6e4403350296bb04bc6
Anchangwan <56478474+ANchangwan@users.noreply.github.com>
2021-10-24 19:15:13 +0900

    Update 실패율2.py

--- This_is_CodingTest/정렬/실패율2.py
+++ This_is_CodingTest/정렬/실패율2.py
@@ -17,3 +17,19 @@ def solution(N, stages):
     answer = [i[0] for i in answer]
     return answer
 
+# 2
+def solution(N, stages):
+    answer = []
+    size = len(stages)
+    result = []
+    for i in range(1,N+1):
+        fail = stages.count(i)
+        if fail:
+            result.append((i, fail/size))
+            size -= fail
+        else:
+            result.append((i, 0))
+    
+    result = sorted(result, key= lambda x:-x[1])
+    answer = [i[0] for i in result]
+    return answer
